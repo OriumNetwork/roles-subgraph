19f4c149c9fb15f4fa603b403a92037f0f82741c
Daniel Lima <daniel.lima@orium.network>
2023-12-12 17:46:08 -0300

    ON-553: refactor

--- src/sftRolesRegistry/index.ts
+++
@@ -1,3 +0,0 @@
-export { handleRoleGranted } from './role-granted-handler'
-export { handleRoleRevoked } from './role-revoked-handler'
-export { handleRoleApprovalForAll } from './role-approval-for-all-handler'
